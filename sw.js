640267ae539cf824eec291a1d6760dd24d6e1ec2
inadain <t.inada1988@gmail.com>
2026-07-12 01:03:21 +0900

    sw: bump version to 202607120103

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Village Aura Service Worker - 202607120058 */
-const CACHE_VER='202607120058';
+/* Village Aura Service Worker - 202607120103 */
+const CACHE_VER='202607120103';
 self.addEventListener('install',function(){self.skipWaiting();});
 self.addEventListener('activate',function(e){
   e.waitUntil(caches.keys().then(function(keys){
